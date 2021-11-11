9736a32b495b70f1064723c9e9e91e4bca5a5f44
Phillip Hansen <phille04@outlook.com>
2021-11-11 12:54:55 +0100

    Added more comments.

--- market/forms.py
+++ market/forms.py
@@ -6,15 +6,16 @@ from wtforms import StringField, PasswordField, SubmitField # This imports the d
 from wtforms.validators import Length, DataRequired, EqualTo, Email # This imports the validators that we use in our form to make sure that the user inputs the correct data.
 
 #* This is the code needed to create our registration on our forms.
+#* This defines that it is form created with FlaskForm.
 class RegisterForm(FlaskForm):
     #* Length is the length that the submitted data must have, DataRequired means that something must have been entered in that field, and EqualTo means that the entered data must be equal to the data entered in the field that the validator checks.
-    # This creates the field where the user inputs the username they desire for their account, because of the validators it needs to be between 2 and 30 characters long, and data must be entered otherwise an error occurs.
+    # This creates the StringField (text field) where the user inputs the username they desire for their account, because of the validators it needs to be between 2 and 30 characters long, and data must be entered otherwise an error occurs.
     username = StringField(label="User Name: ", validators=[Length(min=2, max=30), DataRequired()])
-    # This creates the field where the user inputs their email address, and because of the validators it needs to be a valid email, and data must be entered.
+    # This creates the StringField (text field) where the user inputs their email address, and because of the validators it needs to be a valid email, and data must be entered.
     email_address = StringField(label="Email Address: ", validators=[Email(), DataRequired()])
-    # This creates the field where the user inputs their desired password, and because of the validators it needs to have a minimum length of 6 characters and as before, data must be entered.
+    # This creates the PasswordField (a field which hides the data that was entered with asterisks) where the user inputs their desired password, and because of the validators it needs to have a minimum length of 6 characters and as before, data must be entered.
     password1 = PasswordField(label="Password: ", validators=[Length(min=6), DataRequired()])
-    # This creates the field where the user must re enter their password, and because of the validators it needs to be the same as the previous password, and data must be entered.
+    # This creates the PasswordField (a field which hides the data that was entered with asterisks) where the user must re enter their password, and because of the validators it needs to be the same as the previous password, and data must be entered.
     password2 = PasswordField(label="Confirm Password: ", validators=[EqualTo("password1"), DataRequired()])
-    # This creates a submit button that when clicked will check if the data meets the requirements, and then send the data to the database.
+    # This creates a SubmitField that when clicked will send (POST) the data to the database.
     submit = SubmitField(label="Create Account")
--- market/models.py
+++ market/models.py
@@ -5,12 +5,17 @@ from market import db # This imports our database from the market folder.
 from market import bcrypt # This imports bcrypt from our market folder so that we can encrypt passwords.
 
 #* This creates a users account as well as their current budget.
+#* This defines that a User Table should be added to the database through (db.Model).
 class User(db.Model):
-    # 
+    # This creates an integer column for the id in the database, and it is the primary_key.
     id = db.Column(db.Integer(), primary_key=True)
+    # This creates a string column for the username in the database, and it can have a max length of 30, cannot be null (empty) and has to be unique.
     username = db.Column(db.String(length=30), nullable=False, unique=True)
+    # This creates a string column for the email address in the database, it can have a max length of 50, cannot be null (empty) and has to be unique.
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    # This creates a string column for the password in the database, it can have a max length of 60, and cannot be null (empty).
     password_hash = db.Column(db.String(length=60), nullable=False)
+    # This creates an integer column for the budget in the database, it cannot be null (empty), and has a default (starting) value of 1000.
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     item = db.relationship("Item", backref="owned_user", lazy=True)
     @property

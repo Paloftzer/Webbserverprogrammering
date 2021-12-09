e6be0d07676ac173328698998d754260cb84c59f
Phillip Hansen <phille04@outlook.com>
2021-12-09 11:17:47 +0100

    Removed dynamic key because I realized that it would be problematic for cookies, however I made it longer so t for security

--- market/__init__.py
+++ market/__init__.py
@@ -5,12 +5,11 @@ from flask import Flask #* This imports Flask from the flask library.
 from flask_sqlalchemy import SQLAlchemy #* This imports SQLAlchemy which handles our database.
 from flask_bcrypt import Bcrypt #* This imports the Bcrypt library which is used to encrypt our passwords.
 from flask_login import LoginManager #* This imports the flask login library which is used to allow login.
-from market.gen_key import key
 
 #* This creates and initializes our server and database.
 app = Flask(__name__) # This initializes our flask application from the library.
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db" # This configures our database and gives it a name.
-app.config["SECRET_KEY"] = key # This is the key we use to access the database.
+app.config["SECRET_KEY"] = "a63bf5d1dd17e3dd9da035c1ddab62f88e9a7819e8f3b88a" # This is the key we use to access the database.
 db = SQLAlchemy(app) # This initializes our SQLAlchemy instance.
 bcrypt = Bcrypt(app) # This initializes our Bcrypt instance.
 login_manager = LoginManager(app) # This initializes our LoginManager instance.
